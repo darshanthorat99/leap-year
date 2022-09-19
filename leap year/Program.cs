c65ba42c48ae9c2982ad92e7d966d92a2d79b523
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-19 18:07:08 +0530

    Add project files.

---
+++ leap year/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ente the year");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("it is leap year");
+            }
+            else
+            {
+                Console.WriteLine("it is not a leap year");
+
+            }
+            
+        }
+    }
+}
